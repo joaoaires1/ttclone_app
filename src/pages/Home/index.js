6fe765eb5e8934281c5885a053800eed6f9471a5
João Aires <airesjoao25@outlook.com>
2020-03-28 13:24:56 -0300

    fix loop in  useeffect

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,19 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { StatusBar, View, Text, TextInput, Button, TouchableOpacity } from 'react-native';
 import CustomHeader from '../../components/CustomHeader';
 import { UserContext } from '../../contexts/UserContext';
 import { getData } from '../../utils/helpers';
 
 const Home = ({ navigation }) => {
-    const context = useContext(UserContext);
-    const user = context.user
+    const { user, setUser } = useContext(UserContext);
+    const update = '';
 
+    useEffect(() => {
+
+        getData().then(data => setUser(data));
+
+    }, [update]);
+    
     return (
         <>
             <StatusBar barStyle="light-content" backgroundColor="#ccc" />

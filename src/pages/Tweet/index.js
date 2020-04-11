9749c95df6887fcfaa540973ca6486dd776c3d2b
João Aires <airesjoao25@outlook.com>
2020-04-10 22:14:18 -0300

    Tweet page layout

--- src/pages/Tweet/index.js
+++ src/pages/Tweet/index.js
@@ -1,10 +1,63 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useContext, useState } from 'react';
+import { 
+    View, 
+    TouchableOpacity, 
+    Text, 
+    Image, 
+    KeyboardAvoidingView, 
+    TouchableWithoutFeedback, 
+    Keyboard, 
+    TextInput,
+    Dimensions 
+} from 'react-native';
 import styles from './styles';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { UserContext } from '../../contexts/UserContext';
 
-const Tweet = () => {
+const width = Dimensions.get('window').width - 50;
+
+const Tweet = ({ navigation }) => {
+    console.log(navigation);
+    
+    const { user } = useContext(UserContext);
+    const [ text, setText ] = useState('');
+
     return(
-        <View></View>
+        <View  style={styles.container}>
+            <View style={styles.tweetHeader}>
+                <TouchableOpacity style={{ width: 40, height: 40, justifyContent: "center" }}
+                    onPress={() => navigation.goBack()}
+                >
+                    <Icon name='window-close' size={25} color='#107c10' />
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.tweetBtn}>
+                    <Text style={styles.tweetBtnText}>Tweet</Text>
+                </TouchableOpacity>
+            </View>
+
+            <View style={styles.textContainer}>
+                <Image 
+                    source={{ uri: user.avatar }} 
+                    style={styles.textAvatar}
+                />
+
+                <KeyboardAvoidingView>
+                    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                        <View style={{ width: width }}>
+                            <TextInput
+                                style={{ fontSize: 16, color: '#000' }}
+                                multiline={true}
+                                maxLength={140}
+                                placeholder="What is happening?"
+                                placeholderTextColor='#000'
+                                onChangeText={text => setText(text)}
+                            />
+                        </View>
+                    </TouchableWithoutFeedback>
+                </KeyboardAvoidingView>
+            </View>
+        </View>
     );
 }
 
--- src/pages/Tweet/styles.js
+++ src/pages/Tweet/styles.js
@@ -1,5 +1,34 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
-
+    container: { 
+        marginHorizontal: 10 
+    },
+    tweetHeader: { 
+        height: 50, 
+        flexDirection: 'row',
+        alignItems: 'center', 
+        justifyContent: 'space-between' 
+    },
+    tweetBtn: {
+        width: 72,
+        height: 30,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: '#107c10',
+        borderRadius: 50
+    },
+    tweetBtnText: { 
+        color: '#FFF', 
+        fontWeight: 'bold' 
+    },
+    textContainer: { 
+        flexDirection: 'row' 
+    },
+    textAvatar: {
+        width: 30,
+        height: 30,
+        borderRadius: 50,
+        marginTop: 15
+    }
 });
